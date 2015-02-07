9058aea411c5dcef96ddf2f75a0219316bb83436
chrisn <chris@thinktopic.com>
2015-02-07 15:05:38 -0700

    Got a gl view working, need a few more variables.

--- code/ImageThing/Program.fs
+++ code/ImageThing/Program.fs
@@ -3,6 +3,8 @@
 open Gtk
 open System
 open Glade
+open OpenTK
+open OpenTK.Graphics.OpenGL
 
 type private GladeObj () =
     [<Widget>] [<DefaultValue>] val mutable mainwindow : Window
@@ -13,6 +15,8 @@ type private GladeObj () =
     [<Widget>] [<DefaultValue>] val mutable destination : Entry
     [<Widget>] [<DefaultValue>] val mutable renderview : Viewport
     [<Widget>] [<DefaultValue>] val mutable resultview : Viewport
+    [<Widget>] [<DefaultValue>] val mutable destBrowse : Button
+    [<Widget>] [<DefaultValue>] val mutable srcBrowse : Button
 
     
 
@@ -23,7 +27,23 @@ let main argv =
     let gxml = new Glade.XML( null, "gui.glade", "mainwindow", null )
     let gobj = new GladeObj()
     gxml.Autoconnect( gobj )
+    let renderer = new GLWidget()
+    let results = new Image()
+    gobj.renderview.Add( renderer )
+
+    let renderFrame evArgs = 
+        let mutable width = 0
+        let mutable height = 0
+        gobj.renderview.GetSizeRequest(ref width, ref height)
+        GL.Viewport( 0, 0, width, height)
+        GL.ClearColor( 1.0f, 0.0f, 0.0f, 1.0f )
+        GL.Clear( ClearBufferMask.ColorBufferBit )
 
 
+    let toolkit = Toolkit.Init()
+    renderer.RenderFrame.Add( renderFrame )
+    gobj.renderview.ShowAll();
+    gobj.resultview.Add( results )
+    gobj.resultview.ShowAll();
     Application.Run()
     0 // return an integer exit code
